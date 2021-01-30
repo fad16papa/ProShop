f4345edd37baa9ca4ee6e39a3d68c9d789d1b7d1
Francis Decena <francisdecena@Franciss-MacBook-Pro.local>
2021-01-30 16:53:00 +0800

    Done Creating Basic Structure for FrontEnd App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <Fragment>
-      <h1>Welcome to ProShop</h1>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
   );
 }
 
