d6a440bafedaee7ded6e435951e6a4ea0e9b2259
Francis Decena <fad16papa@gmail.com>
2021-02-13 09:39:50 +0800

    Commit changes Section 12

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,18 +2,14 @@ import express from "express";
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-//@desc:    This will fetch all the products
-//@route:   GET /api/products
-//@access:  Public
 router.route("/").get(getProducts);
 
-//@desc:    This will fetch single product by its ID
-//@route:   GET /api/products/:id
-//@access:  Public
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
