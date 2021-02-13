3072eed834d6c7aa2c834eceacc8c55ab4859f50
Francis Decena <fad16papa@gmail.com>
2021-02-13 22:09:59 +0800

    Commit changes section 13

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoute.js";
@@ -15,6 +16,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("API is running...");
