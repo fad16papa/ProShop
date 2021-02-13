ffda361a9e6170362246218660682b11a049343b
Francis Decena <fad16papa@gmail.com>
2021-02-13 12:52:24 +0800

    Commit changes

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -42,6 +42,25 @@ const deleteProduct = asyncHandler(async (req, res) => {
 //@route:   POST /api/products/
 //@access:  Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+//@desc:    Update a product
+//@route:   Put /api/products/:id
+//@access:  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
   const {
     name,
     price,
@@ -55,12 +74,26 @@ const createProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    const createdProduct = await product.save();
-    res, status(201).json(product);
+    (product.name = name),
+      (product.price = price),
+      (product.description = description),
+      (product.image = image),
+      (product.brand = brand),
+      (product.category = category),
+      (product.countInStock = countInStock);
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error("Product not found");
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -36,6 +37,10 @@ function App() {
             <Route path='/cart/:id?' component={CartScreen} />
             <Route path='/admin/userlist' component={UserListScreen} />
             <Route path='/admin/productlist' component={ProductListScreen} />
+            <Route
+              path='/admin/product/:id/edit'
+              component={ProductEditScreen}
+            />
             <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           </Container>
         </main>
