ec4dad518ece2188947bf309c034dbbe33524c31
fad16papa <fad16papa@gmail.com>
2021-02-23 16:20:18 +0800

    Commit changes\

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -33,35 +33,36 @@ const LoginScreen = ({ location, history }) => {
   return (
     <FormContainer>
       <h1>Sign In</h1>
-      {error && <Message variant='danger'>{error}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId='email'>
+        <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <FormControl
-            type='email'
-            placeholder='Enter email'
+            type="email"
+            placeholder="Enter email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}></FormControl>
+            onChange={(e) => setEmail(e.target.value)}
+          ></FormControl>
         </Form.Group>
 
-        <Form.Group controlId='password'>
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <FormControl
-            type='password'
-            placeholder='Enter password'
+            type="password"
+            placeholder="Enter password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}></FormControl>
+            onChange={(e) => setPassword(e.target.value)}
+          ></FormControl>
         </Form.Group>
-        <Button type='submit' variant='primary'>
+        <Button type="submit" variant="primary">
           Sign In
         </Button>
       </Form>
-
-      <Row className='py-3'>
+      <Row className="py-3">
         <Col>
           New Customer?{" "}
-          <Link to={redirect ? `/register?redirect=${redirect}` : "/regsiter"}>
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
             Register
           </Link>
         </Col>
